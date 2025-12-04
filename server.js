3e049c107de3f25171411985fcea64143fc4c5ba
MargaretteG <167627867+MargaretteG@users.noreply.github.com>
2025-12-04 04:12:11 +0000

    add package.json

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+
+  let filePath = '.' + req.url;
+  if (filePath === './') filePath = './index.html';
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.css':  'text/css',
+    '.js':   'text/javascript',
+    '.json': 'application/json',
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found!');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
